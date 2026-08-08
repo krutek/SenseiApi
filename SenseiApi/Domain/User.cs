cf7317b4b02bc9ed6a2ee6a9a345b5f0d030abbf
blampricht <blampricht@10572-TPS>
2026-08-08 10:40:28 +0200

    Add user LastName

--- SenseiApi/Domain/User.cs
+++ SenseiApi/Domain/User.cs
@@ -3,22 +3,23 @@
     public class User
     {
         public Guid Id { get; private set; }
-        public string Name { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
         public string PasswordHash { get; private set; }
         public string Email { get; private set; }
         public DateTime CreatedAt { get; private set; }
 
         private User() { } // dla EF Core
 
-        public User(string name, string email, string passwordHash)
+        public User(string firstName, string lastName, string email, string passwordHash)
         {
             Id = Guid.NewGuid();
-            Name = name;
+            FirstName = firstName;
+            LastName = lastName;
             Email = email;
             PasswordHash = passwordHash;
             CreatedAt = DateTime.UtcNow;
         }
-
 
         public void ChangeEmail(string email)
         {
